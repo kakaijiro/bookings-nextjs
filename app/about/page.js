3c029376e35833bbfa9a3f72d5535279bd21f0d1
kakaijiro <kakaijiro@gmail.com>
2025-04-25 19:31:06 +0900

    Add header, navigation, and logo components; implement cabin data service and update styles

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "About",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>about us</h1>
+    </div>
+  );
+}
---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Guest area",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Account</h1>
+    </div>
+  );
+}
